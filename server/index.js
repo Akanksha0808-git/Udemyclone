d9a80c4f6e50583b93e63cfa24d738e74c270d9b
Akanksha <akanksharma0829@gmail.com>
2023-12-02 23:04:34 +0530

    commiting changes

--- server/index.js
+++ server/index.js
@@ -238,8 +238,19 @@ const data=[
 },
 {
     id:'26',
-    category:'Buisness',
-    subcategory:'Human Resources',
+    category:'Finance and Accounting',
+    subcategory:'Accoutinf & Bookkeeping',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'27',
+    category:'Finance and Accounting',
+    subcategory:'Accoutinf & Bookkeeping',
     heading:'',
     img:'',
     author:'',
@@ -247,15 +258,1143 @@ const data=[
     price:''
 },
 {
-    id:'26',
-    category:'Buisness',
-    subcategory:'Human Resources',
+    id:'28',
+    category:'Finance and Accounting',
+    subcategory:'Accoutinf & Bookkeeping',
     heading:'',
     img:'',
     author:'',
     rating:'',
     price:''
 },
+{
+    id:'29',
+    category:'Finance and Accounting',
+    subcategory:'Accoutinf & Bookkeeping',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'30',
+    category:'Finance and Accounting',
+    subcategory:'Accoutinf & Bookkeeping',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'31',
+    category:'Finance and Accounting',
+    subcategory:'Compilance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'32',
+    category:'Finance and Accounting',
+    subcategory:'Compilance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'33',
+    category:'Finance and Accounting',
+    subcategory:'Compilance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'34',
+    category:'Finance and Accounting',
+    subcategory:'Compilance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'35',
+    category:'Finance and Accounting',
+    subcategory:'Compilance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'36',
+    category:'Finance and Accounting',
+    subcategory:'Economic',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+
+,
+{
+    id:'37',
+    category:'Finance and Accounting',
+    subcategory:'Economic',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'38',
+    category:'Finance and Accounting',
+    subcategory:'Economic',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'39',
+    category:'Finance and Accounting',
+    subcategory:'Economic',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'40',
+    category:'Finance and Accounting',
+    subcategory:'Economic',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'41',
+    category:'Finance and Accounting',
+    subcategory:'Finance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'42',
+    category:'Finance and Accounting',
+    subcategory:'Finance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'43',
+    category:'Finance and Accounting',
+    subcategory:'Finance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'44',
+    category:'Finance and Accounting',
+    subcategory:'Finance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'45',
+    category:'Finance and Accounting',
+    subcategory:'Finance',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'46',
+    category:'Finance and Accounting',
+    subcategory:'Taxes',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'47',
+    category:'Finance and Accounting',
+    subcategory:'Taxes',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'48',
+    category:'Finance and Accounting',
+    subcategory:'Taxes',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'49',
+    category:'Finance and Accounting',
+    subcategory:'Taxes',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'50',
+    category:'Finance and Accounting',
+    subcategory:'Taxes',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'51',
+    category:'IT&Software',
+    subcategory:'IT Certificates',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'52',
+    category:'IT&Software',
+    subcategory:'IT Certificates',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'53',
+    category:'IT&Software',
+    subcategory:'IT Certificates',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'54',
+    category:'IT&Software',
+    subcategory:'IT Certificates',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'55',
+    category:'IT&Software',
+    subcategory:'IT Certificates',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'56',
+    category:'IT&Software',
+    subcategory:'Network&Security',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'57',
+    category:'IT&Software',
+    subcategory:'Network&Security',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'58',
+    category:'IT&Software',
+    subcategory:'Network&Security',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'59',
+    category:'IT&Software',
+    subcategory:'Network&Security',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'60',
+    category:'IT&Software',
+    subcategory:'Network&Security',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'61',
+    category:'IT&Software',
+    subcategory:'Hardware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'62',
+    category:'IT&Software',
+    subcategory:'Hardware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'63',
+    category:'IT&Software',
+    subcategory:'Hardware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'64',
+    category:'IT&Software',
+    subcategory:'Hardware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'65',
+    category:'IT&Software',
+    subcategory:'Hardware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'66',
+    category:'IT&Software',
+    subcategory:'OS & Servers',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'67',
+    category:'IT&Software',
+    subcategory:'OS & Servers',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'68',
+    category:'IT&Software',
+    subcategory:'OS & Servers',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'69',
+    category:'IT&Software',
+    subcategory:'OS & Servers',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'70',
+    category:'IT&Software',
+    subcategory:'OS & Servers',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'71',
+    category:'IT&Software',
+    subcategory:'Other IT & Sotware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'72',
+    category:'IT&Software',
+    subcategory:'Other IT & Sotware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'73',
+    category:'IT&Software',
+    subcategory:'Other IT & Sotware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'74',
+    category:'IT&Software',
+    subcategory:'Other IT & Sotware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'75',
+    category:'IT&Software',
+    subcategory:'Other IT & Sotware',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'77',
+    category:'Design',
+    subcategory:'Web Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'78',
+    category:'Design',
+    subcategory:'Web Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'79',
+    category:'Design',
+    subcategory:'Web Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'80',
+    category:'Design',
+    subcategory:'Web Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'81',
+    category:'Design',
+    subcategory:'Game Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'82',
+    category:'Design',
+    subcategory:'Game Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'83',
+    category:'Design',
+    subcategory:'Game Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'84',
+    category:'Design',
+    subcategory:'Game Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'85',
+    category:'Design',
+    subcategory:'Game Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'86',
+    category:'Design',
+    subcategory:'3D & Animation',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'87',
+    category:'Design',
+    subcategory:'3D & Animation',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'88',
+    category:'Design',
+    subcategory:'3D & Animation',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'89',
+    category:'Design',
+    subcategory:'3D & Animation',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'90',
+    category:'Design',
+    subcategory:'3D & Animation',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'91',
+    category:'Design',
+    subcategory:'Fashion Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'92',
+    category:'Design',
+    subcategory:'Fashion Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'93',
+    category:'Design',
+    subcategory:'Fashion Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'94',
+    category:'Design',
+    subcategory:'Fashion Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'95',
+    category:'Design',
+    subcategory:'Fashion Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'96',
+    category:'Design',
+    subcategory:'Other Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'97',
+    category:'Design',
+    subcategory:'Other Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'98',
+    category:'Design',
+    subcategory:'Other Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'99',
+    category:'Design',
+    subcategory:'Other Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'100',
+    category:'Design',
+    subcategory:'Other Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'101',
+    category:'Marketing',
+    subcategory:'Digital Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'102',
+    category:'Marketing',
+    subcategory:'Digital Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'103',
+    category:'Marketing',
+    subcategory:'Digital Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'104',
+    category:'Marketing',
+    subcategory:'Digital Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'105',
+    category:'Marketing',
+    subcategory:'Digital Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'106',
+    category:'Marketing',
+    subcategory:'Social Media Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'107',
+    category:'Marketing',
+    subcategory:'Social Media Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'108',
+    category:'Marketing',
+    subcategory:'Social Media Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'109',
+    category:'Marketing',
+    subcategory:'Social Media Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'110',
+    category:'Marketing',
+    subcategory:'Social Media Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'111',
+    category:'Marketing',
+    subcategory:'Public Relationship',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'112',
+    category:'Marketing',
+    subcategory:'Public Relationship',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'113',
+    category:'Marketing',
+    subcategory:'Public Relationship',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'114',
+    category:'Marketing',
+    subcategory:'Public Relationship',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'115',
+    category:'Marketing',
+    subcategory:'Public Relationship',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'116',
+    category:'Marketing',
+    subcategory:'Content Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'117',
+    category:'Marketing',
+    subcategory:'Content Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'118',
+    category:'Marketing',
+    subcategory:'Content Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'119',
+    category:'Marketing',
+    subcategory:'Content Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'120',
+    category:'Marketing',
+    subcategory:'Content Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'121',
+    category:'Marketing',
+    subcategory:'Product Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'122',
+    category:'Marketing',
+    subcategory:'Product Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'123',
+    category:'Marketing',
+    subcategory:'Product Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'124',
+    category:'Marketing',
+    subcategory:'Product Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'125',
+    category:'Marketing',
+    subcategory:'Product Marketing',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'126',
+    category:'Photography & Video',
+    subcategory:'Digital Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'127',
+    category:'Photography & Video',
+    subcategory:'Digital Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'128',
+    category:'Photography & Video',
+    subcategory:'Digital Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'129',
+    category:'Photography & Video',
+    subcategory:'Digital Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'130',
+    category:'Photography & Video',
+    subcategory:'Digital Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'131',
+    category:'Photography & Video',
+    subcategory:'Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'132',
+    category:'Photography & Video',
+    subcategory:'Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'133',
+    category:'Photography & Video',
+    subcategory:'Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'134',
+    category:'Photography & Video',
+    subcategory:'Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+,
+{
+    id:'135',
+    category:'Photography & Video',
+    subcategory:'Photography',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'136',
+    category:'Photography & Video',
+    subcategory:'Video Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'137',
+    category:'Photography & Video',
+    subcategory:'Video Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'138',
+    category:'Photography & Video',
+    subcategory:'Video Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'139',
+    category:'Photography & Video',
+    subcategory:'Video Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+},
+{
+    id:'140',
+    category:'Photography & Video',
+    subcategory:'Video Design',
+    heading:'',
+    img:'',
+    author:'',
+    rating:'',
+    price:''
+}
+
+
 
 
 
